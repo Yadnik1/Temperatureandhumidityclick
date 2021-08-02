da18d802340a65fa624523f38e802a3d363bb7d2
Yadnik Bendale <66904635+Yadnik1@users.noreply.github.com>
2021-08-02 19:03:38 +0530

    Update T&HClick_HTU21DF.py

--- T&HClick_HTU21DF.py
+++ T&HClick_HTU21DF.py
@@ -1,6 +1,6 @@
 from machine import I2C, Pin
 import time
-import os
+
 
 device = 0x40
 regAddresstemp = 0xE3
@@ -13,28 +13,30 @@ class HTU21DF():
         self.addr = addr
         self.i2c = i2c
         b = bytearray(1) 
-        b[0] = 0
-        self.i2c.writeto_mem(self.addr,0xE6,b) #0x2d is POWER_CTL register
-        b[0] = 16
-        self.i2c.writeto_mem(self.addr,0xE6,b) #0x2d is POWER_CTL register
+        b[0] = 0 convert 
+        self.i2c.writeto_mem(self.addr,0xE6,b) #enabled sensor ic by writing to control register 
         b[0] = 8
-        self.i2c.writeto_mem(self.addr,0xE6,b) #0x2d is POWER_CTL register
+        self.i2c.writeto_mem(self.addr,0xE6,b) #0xE6 is POWER_CTL register
+        
         
     def temperature(self):
-        buff = self.i2c.readfrom_mem(self.addr,regAddresstemp,TO_READ)
-        temp=(buff[0] << 8) + buff[0]
-        return -46.85 + (175.72 * temp / 65536)
+        buff = self.i2c.readfrom_mem(self.addr,regAddresstemp,TO_READ) #read 6 bytes of data from register address
+        temp=(buff[0] << 8) + buff[0] #Parse data
+        return -46.85 + (175.72 * temp / 65536) #formula from datasheet
     
     def humidity(self):
-        buff = self.i2c.readfrom_mem(self.addr,regAddresshum,TO_READ)
-        hum=(buff[0] << 8) + buff[0]
-        return -6 + (125.0 * hum/ 65536)
+        buff = self.i2c.readfrom_mem(self.addr,regAddresshum,TO_READ) #read 6 bytes of data from register address
+        hum=(buff[0] << 8) + buff[0] #Parse data
+        return -6 + (125.0 * hum/ 65536) #formula from datasheet
     
-i2c = I2C("I2C_0")
+i2c = I2C("I2C_0") #Initialise I2C class
 tandh=HTU21DF(i2c)
 while True:
     t=tandh.temperature()
     h=tandh.humidity()
-    print(h,t)
+    print("HUMIDITY = %d rh \t TEMPERATURE = %d °C" % (h,t))
     
+
+
+
 
